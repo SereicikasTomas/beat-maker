e85d3f35f1191e5c861e9843dc169d936885f54d
jacando.tos <sereicikas.t@gmail.com>
2021-03-04 17:52:36 +0100

    initial ts setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,48 @@
-console.log('test');
+class DrumKit {
+  pads;
+  playButton;
+  kickSound;
+  snareSound;
+  hihatSound;
+  index;
+  beatsPerMinute;
+
+  constructor() {
+    this.pads = document.querySelectorAll('.pad') as NodeListOf<Element>;
+    this.playButton = document.querySelector('.play') as HTMLElement;
+    this.kickSound = document.querySelector('.kick-sound') as HTMLElement;
+    this.snareSound = document.querySelector('.snare-sound') as HTMLElement;
+    this.hihatSound = document.querySelector('.hihat-sound') as HTMLElement;
+    this.index = 0;
+    this.beatsPerMinute = 120;
+  }
+
+  activePad() {
+    this.classList.toggle('active');
+  }
+
+  repeat() {
+    // get the remainder so that on the last value it resets to zero
+    let step = this.index % (this.pads.length / 3);
+    const acitveBar = document.querySelectorAll(`.b${step}`);
+    this.index++;
+    console.log(step);
+  }
+
+  play() {
+    const interval = (60 / this.beatsPerMinute) * 1000;
+    setInterval(() => {
+      this.repeat();
+    }, interval);
+  }
+}
+
+const drumKit = new DrumKit();
+
+drumKit.pads.forEach(pad => {
+  pad.addEventListener('click', drumKit.activePad);
+})
+
+drumKit.playButton.addEventListener('click', () => {
+  drumKit.play();
+});
